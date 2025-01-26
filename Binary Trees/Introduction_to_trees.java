374958d1cc8ef74bff4f15e91deacd4be28d6934
B-V-SAI-PRANAV <bvsaip@gmail.com>
2025-01-26 22:26:53 +0530

    Introduction_to_trees.java

--- Binary Trees/Introduction_to_trees.java
+++ Binary Trees/Introduction_to_trees.java
@@ -2,7 +2,7 @@ Print the maximum number of nodes on level i of a binary tree.
 
 
 
-class Solution {
+class Introductiontotrees {
     static int countNodes(int i) {
         // code here
         return  (int)Math.pow(2,i-1);
