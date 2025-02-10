9b34c84ec5c049f1c44f20373b4b9102213adf6e
B-V-SAI-PRANAV <bvsaip@gmail.com>
2025-02-10 21:39:51 +0530

    Create jump-game.java

---
+++ Greedy-Algorithms/jump-game.java
@@ -0,0 +1,17 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int maxIndex=0;
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            if(maxIndex>n){
+                return true;
+            }
+            if(i>maxIndex){
+                return false;
+            }
+            maxIndex=Math.max(maxIndex,i+nums[i]);
+            
+        }
+        return true ;
+    }
+}
