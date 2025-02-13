b83c8c35118acdc1243b723c7686181ee9909f26
B-V-SAI-PRANAV <bvsaip@gmail.com>
2025-02-13 18:29:09 +0530

    Create BalancedBinaryTree.java

---
+++ Binary Trees/BalancedBinaryTree.java
@@ -0,0 +1,24 @@
+class BalancedBinaryTree {
+    public boolean isBalanced(TreeNode root) {
+        return helper(root)!=-1;
+    }
+    private int helper(TreeNode node){
+        if(node==null){
+            return 0;
+        }
+        int leftHeight=helper(node.left);
+        if(leftHeight==-1){
+            return -1;
+        }
+        int rightHeight=helper(node.right);
+        if(rightHeight==-1){
+            return -1;
+        }
+
+        if(Math.abs(leftHeight-rightHeight)>1){
+            return -1;
+        }
+
+        return Math.max(leftHeight,rightHeight) +1;
+    }
+}
