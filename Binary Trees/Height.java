753ff1d08451da958d218ea7f0bec994de2010e1
B-V-SAI-PRANAV <bvsaip@gmail.com>
2025-02-13 18:28:11 +0530

    Create Height.java

---
+++ Binary Trees/Height.java
@@ -0,0 +1,9 @@
+class Height {
+ 
+    public int maxDepth(TreeNode node) {
+        if (node == null) {
+            return 0;
+        }
+        return Math.max(maxDepth(node.left), maxDepth(node.right)) + 1;
+    }
+}
