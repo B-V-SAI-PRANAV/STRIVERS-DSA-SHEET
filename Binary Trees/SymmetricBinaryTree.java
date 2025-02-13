06cbf7f888627dcc0cb990a4ab0349c97e11bc23
B-V-SAI-PRANAV <bvsaip@gmail.com>
2025-02-13 18:48:54 +0530

    Create SymmetricBinaryTree.java

---
+++ Binary Trees/SymmetricBinaryTree.java
@@ -0,0 +1,14 @@
+class SymmetricBinaryTree {
+    public boolean isSymmetric(TreeNode root) {
+    return helper(root.left,root.right);
+    }
+    private boolean helper(TreeNode left,TreeNode right){
+        if(left==null || right==null){
+            return left==right;
+        }
+        if(left.val!=right.val){
+            return false;
+        }
+        return helper(left.left,right.right) && helper(left.right,right.left);
+    }
+}
